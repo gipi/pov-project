c40f9df9391b0a2d2f4f9b015386c782f1c3ab74
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2015-10-31 16:11:50 +0100

    Timer1 overflow.

--- firmware/pov.cpp
+++ firmware/pov.cpp
@@ -12,6 +12,15 @@
 #include <avr/power.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#ifdef __AXX__
+#include <Wire.h>
+#include <EEPROM.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+#endif
+
+//#define TEST
+
 
 /*
  * From "making a glyph from bit patterns" chapter of "Expert C
@@ -26,14 +35,31 @@
 #define s ((((((((0 /* For building pattern 8 bit wide */
 
 const static unsigned char pattern[] = {
+#if 0
     s _ _ _ X X _ _ _,
-    s _ X X _ _ X X _,
-    s X _ _ _ _ _ _ X,
-    s _ _ X _ _ X _ _,
+    s _ _ X X X X _ _,
+    s _ X X X X X X _,
     s X X X X X X X X,
-    s X _ _ _ _ _ _ X,
-    s X _ X _ X _ X _,
-    s _ X _ X _ X _ X,
+    s _ _ X X X X _ _,
+    s _ _ X X X X _ _,
+    s _ _ X X X X _ _,
+    s _ _ X X X X _ _,
+#else
+    s X _ _ _ _ _ _ _,
+    s _ X _ _ _ _ _ _,
+    s _ _ X _ _ _ _ _,
+    s _ _ _ X _ _ _ _,
+    s _ _ _ _ X _ _ _,
+    s _ _ _ _ _ X _ _,
+    s _ _ _ _ _ _ X _,
+    s _ _ _ _ _ _ _ X,
+    s _ _ _ _ _ _ X _,
+    s _ _ _ _ _ X _ _,
+    s _ _ _ _ X _ _ _,
+    s _ _ _ X _ _ _ _,
+    s _ _ X _ _ _ _ _,
+    s _ X _ _ _ _ _ _,
+#endif
 };
 
 #undef X
@@ -76,8 +102,6 @@ enum led_state_t {
     ON,
     OFF
 };
-
-
 
 void set_led(unsigned short idx, enum led_state_t state) {
     struct led_t led = leds[idx];
@@ -126,37 +150,105 @@ void display_bit_pattern(uint8_t digit, uint8_t size) {
     }
 }
 
-unsigned short g_index = 0;
+uint8_t g_index = 0;
 
 void display_next() {
     // display the row
     display_bit_pattern(pattern[g_index++ % sizeof(pattern)], 8);
 }
 
+#ifdef __AXX__
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+
+/************* EEPROM **********************/
 /**
+ * Register in the EEPROM the accelerometer data.
+ */
+struct _acc_history {
+    uint8_t min;
+    uint8_t max;
+    uint16_t idx;
+} acc_history;
+
+void update_acc_register(sensors_event_t event) {
+    uint8_t actual_x_value = event.acceleration.x;
+    if (event.acceleration.x < acc_history.min ) {
+        acc_history.min = actual_x_value;
+    }
+    if (event.acceleration.x > acc_history.max ) {
+        acc_history.max = actual_x_value;
+    }
+
+    // save the data into the eeprom
+    EEPROM.write(0, acc_history.min);
+    EEPROM.write(1, acc_history.max);
+    EEPROM.write(2 + acc_history.idx, actual_x_value);
+
+    // update the index (1024 is the EEPROM size in the ATMEGA32U4)
+    acc_history.idx = (acc_history.idx + 1) % (1024 - 2);
+}
+#endif
+
+/**
+ * The Timer1 is a 16bits counter.
+ *
+ * An overflow at 16MHz without prescaler is given by
+ *
+ *  16000000/65536 = 244.14Hz
+ *
+ * (We are not using the Timer0 counter since is used by Wire, maybe)
  */
 void init_timer() {
-    TCCR0B |= _BV(CS00) | _BV(CS02); // clk/1024
-    TIMSK0 |= _BV(TOIE0);            // enable Timer0 Overflow Interrupt
+    cli();
+   // TCCR1B |= ~_BV(CS12) | ~_BV(CS11) | _BV(CS10);
+    TCCR1A = 0;
+    TCNT1 = 0;
+    TCCR1B |= _BV(CS12) | ~_BV(CS11) | _BV(CS10);
+    TIMSK1 |= _BV(TOIE1);            // enable Timer1 Overflow Interrupt
+
+    sei();
 }
 
-ISR(TIMER0_OVF_vect) {
+
+ISR(TIMER1_OVF_vect) {
     display_next();
+
+#ifdef __AXX__
+    /* Get a new sensor event */ 
+    sensors_event_t event; 
+    accel.getEvent(&event);
+    update_acc_register(event);
+#endif
+
+    TCNT1 = 0;
 }
+
+
+
 
 int main() {
-    // change to 8MHz
-    //clock_prescale_set(clock_div_1);
-
+    clock_prescale_set(clock_div_1);
 
     init_led_system();
 
     led_self_test();
 
-    init_timer();
-    sei();
+#ifdef __AXX__
+    accel.begin();
 
-    while(1); // needed in order to avoid crash (maybe?)
+    /* Set the range to whatever is appropriate for your project */
+    accel.setRange(ADXL345_RANGE_16_G);
+#endif
+    init_timer();
+
+    while(1)//; // needed in order to avoid crash (maybe?)
+    {
+#ifdef TEST
+        display_next();
+        _delay_ms(1000);
+#endif
+    }
 
     return 0;
 }
