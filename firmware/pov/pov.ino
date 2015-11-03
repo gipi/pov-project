b800f20d3abb2c74a5bbb1bc2f2b0f3efb52783f
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2015-11-03 09:48:13 +0100

    Move main firmware code to ino format.

--- firmware/pov/pov.ino
+++ firmware/pov/pov.ino
@@ -8,18 +8,18 @@
  *  - allow for more "depth" of patterns (monochrome, RGB)
  *  - animations
  */
+//#define TEST
+
 #include <avr/io.h>
 #include <avr/power.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#ifdef __AXX__
 #include <Wire.h>
 #include <EEPROM.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
-#endif
 
-//#define TEST
+static long baud = 57600;
 
 
 /*
@@ -77,7 +77,7 @@ struct led_t {
     unsigned int short port;
 };
 
-struct led_t leds[] = {
+const struct led_t leds[] = {
     {.ddm = &DDRC, .dd = DDC7, .portm = &PORTC, .port = PORTC7},
     {.ddm = &DDRC, .dd = DDC6, .portm = &PORTC, .port = PORTC6},
     {.ddm = &DDRB, .dd = DDB6, .portm = &PORTB, .port = PORTB6},
@@ -89,13 +89,18 @@ struct led_t leds[] = {
 };
 
 // set as output all the leds
+/**
+ * Here we set the direction for the various pins
+ */
 void init_led_system() {
+    Serial1.println("--> init_led_system()");
     unsigned int idx = 0;
 
     for (idx = 0 ; idx < sizeof(leds) ; idx++) {
         struct led_t led = leds[idx];
         *led.ddm |= _BV(led.dd);
     }
+    Serial1.println("<-- init_led_system()\n");
 }
 
 enum led_state_t {
@@ -128,16 +133,22 @@ void set_led_off(unsigned int idx) {
  * Simply loop over all the leds
  */
 void led_self_test() {
+    Serial1.println("--> led_self_test()");
     unsigned int idx;
 
-    for (idx = 0 ; idx < sizeof(leds)/sizeof(struct led_t) ; idx++) {
+    unsigned int size = sizeof(leds)/sizeof(struct led_t);
+
+    Serial1.print("# leds: ");
+    Serial1.println(size);
+
+    for (idx = 0 ; idx < size ; idx++) {
         set_led_on(idx);
-        _delay_ms(DELAY_QUICK_VALUE);
+        delay(DELAY_QUICK_VALUE);
     }
 
     for (idx = 0 ; idx < sizeof(leds)/sizeof(struct led_t) ; idx++) {
         set_led_off(idx);
-        _delay_ms(DELAY_QUICK_VALUE);
+        delay(DELAY_QUICK_VALUE);
     }
 }
 
@@ -150,14 +161,15 @@ void display_bit_pattern(uint8_t digit, uint8_t size) {
     }
 }
 
-uint8_t g_index = 0;
+volatile uint8_t g_index = 0;
 
 void display_next() {
     // display the row
     display_bit_pattern(pattern[g_index++ % sizeof(pattern)], 8);
 }
 
-#ifdef __AXX__
+// DO NOT INITIALIZE HERE OTHERWISE FAIL
+// SINCE (?) CONSTRUCTOR IS CALLED BEFORE MAIN()
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
@@ -171,8 +183,16 @@ struct _acc_history {
     uint16_t idx;
 } acc_history;
 
+void dump_acc_history() {
+    Serial1.print("history\n idx: ");
+    Serial1.print(acc_history.idx);
+    Serial1.print(" min: ");
+    Serial1.println(acc_history.min);
+}
+
 void update_acc_register(sensors_event_t event) {
     uint8_t actual_x_value = event.acceleration.x;
+
     if (event.acceleration.x < acc_history.min ) {
         acc_history.min = actual_x_value;
     }
@@ -181,14 +201,28 @@ void update_acc_register(sensors_event_t event) {
     }
 
     // save the data into the eeprom
-    EEPROM.write(0, acc_history.min);
-    EEPROM.write(1, acc_history.max);
-    EEPROM.write(2 + acc_history.idx, actual_x_value);
+    //EEPROM.write(0, acc_history.min);
+    //EEPROM.write(1, acc_history.max);
+    //EEPROM.write(2 + acc_history.idx, actual_x_value);
 
     // update the index (1024 is the EEPROM size in the ATMEGA32U4)
     acc_history.idx = (acc_history.idx + 1) % (1024 - 2);
 }
-#endif
+
+void timer1_ovf_callback() {
+    display_next();
+
+    sensors_event_t event; 
+    /* Get a new sensor event */ 
+    accel.getEvent(&event);
+    //update_acc_register(event);
+}
+
+ISR(TIMER1_OVF_vect) {
+    timer1_ovf_callback();
+
+    TCNT1 = 0;
+}
 
 /**
  * The Timer1 is a 16bits counter.
@@ -200,48 +234,48 @@ void update_acc_register(sensors_event_t event) {
  * (We are not using the Timer0 counter since is used by Wire, maybe)
  */
 void init_timer() {
+    Serial1.println("init_timer()");
+    Serial1.flush();
     cli();
-   // TCCR1B |= ~_BV(CS12) | ~_BV(CS11) | _BV(CS10);
     TCCR1A = 0;
     TCNT1 = 0;
-    TCCR1B |= _BV(CS12) | ~_BV(CS11) | _BV(CS10);
+    TCCR1B |= _BV(CS11) | _BV(CS10);
     TIMSK1 |= _BV(TOIE1);            // enable Timer1 Overflow Interrupt
 
     sei();
 }
 
 
-ISR(TIMER1_OVF_vect) {
-    display_next();
 
-#ifdef __AXX__
-    /* Get a new sensor event */ 
-    sensors_event_t event; 
-    accel.getEvent(&event);
-    update_acc_register(event);
-#endif
+void setup() {
+    //clock_prescale_set(clock_div_1);
+    delay(3000);// give me time to open the serial console
+    // YOU MUST HAVE THE SERIAL OPEN MOTHERFUCKER
+    Serial1.begin(baud);
+    Serial1.println(__FILE__);
+    Serial1.println("setup()");
 
-    TCNT1 = 0;
-}
-
-
-
-
-int main() {
-    clock_prescale_set(clock_div_1);
+    Serial1.print("sizeof ");
+    Serial1.println(sizeof(leds));
+    Serial1.println(sizeof(struct led_t));
+    dump_acc_history();
 
     init_led_system();
 
     led_self_test();
 
-#ifdef __AXX__
-    accel.begin();
-
+    if(!accel.begin()){
+        Serial1.println("No ADXL345!!");
+        while(1);
+    }
     /* Set the range to whatever is appropriate for your project */
     accel.setRange(ADXL345_RANGE_16_G);
-#endif
-    init_timer();
 
+    init_timer();
+}
+
+void loop() {
+    Serial1.println("loop()");
     while(1)//; // needed in order to avoid crash (maybe?)
     {
 #ifdef TEST
@@ -249,6 +283,4 @@ int main() {
         _delay_ms(1000);
 #endif
     }
-
-    return 0;
 }
